32d8fdfa0f7ce4d3384a9fd9de6bd74526e58e0d
KareemGhorab <kareemghorab3@gmail.com>
2023-07-10 19:59:00 +0300

    [*] Navbar styling

--- components/navigation/navbar/nav-logo.tsx
+++ components/navigation/navbar/nav-logo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function NavLogo({ className = '' }: BasicProps) {
 	return (
 		<figure
-			className={`bg-orange-400 rounded-full h-[170px] w-44 flex justify-center items-center ${className}`}
+			className={`bg-white rounded-full h-[170px] w-44 flex justify-center items-center ${className}`}
 		>
 			<Link href='/'>
 				<Image
