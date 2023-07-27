e78159caa27e5ba010989cb21035f59970e9bbbb
Kareem Ghorab <kareemghorab3@gmail..com>
2023-07-27 22:32:00 +0300

    [+] Basic product card structure

--- components/home/products/products-list.tsx
+++ components/home/products/products-list.tsx
@@ -1,5 +1,16 @@
-export default function ProductsList() {
-	return <>
-		
-	</>
+import { Product } from '@/types'
+import ProductCard from './product-card'
+
+type Props = {
+	products: Product[]
+}
+
+export default function ProductsList({ products }: Props) {
+	return (
+		<ul className='flex gap-5 '>
+			{products.map((product) => (
+				<ProductCard product={product} key={product.name} />
+			))}
+		</ul>
+	)
 }
