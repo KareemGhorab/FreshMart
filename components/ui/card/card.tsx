c6f830fb257cd418178be499ad2bd0e61ac0acaa
KareemGhorab <kareemghorab3@gmail.com>
2023-08-18 15:46:30 +0300

    feat: card container component

--- components/ui/card/card.tsx
+++ components/ui/card/card.tsx
@@ -1,29 +1,13 @@
-import Image from 'next/image'
 import clsx from 'clsx'
-import Rating from '@/components/ui/rating/rating'
-import Price from '@/components/ui/price/price'
-import { TProduct } from '@/types/models/product'
 
 import styles from './card.module.scss'
 
 type TProps = {
-	product: TProduct
 	className?: string
-}
+} & React.PropsWithChildren
 
-const Card = ({
-	product: {
-		name,
-		price,
-		discount,
-		totalRating,
-		image: { src, alt, width, height },
-	},
-	className = '',
-}: TProps) => (
-	<article className={clsx(styles.card, className)}>
-		
-	</article>
+const Card = ({ className, children }: TProps) => (
+	<div className={clsx(styles.card, className)}>{children}</div>
 )
 
 export default Card
