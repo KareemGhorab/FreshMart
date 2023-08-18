b39ea3c861918e961587abc76606a04b27cd371d
KareemGhorab <kareemghorab3@gmail.com>
2023-08-18 14:36:31 +0300

    Added playfair display font

--- components/fonts.ts
+++ components/fonts.ts
@@ -1,6 +1,12 @@
 import { Amatic_SC } from 'next/font/google'
+import { Playfair_Display } from 'next/font/google'
 
 export const amatic_SC = Amatic_SC({
 	weight: ['400', '700'],
+	subsets: ['latin'],
+})
+
+export const playfair_display = Playfair_Display({
+	weight: ['400', '500', '600', '700'],
 	subsets: ['latin'],
 })
