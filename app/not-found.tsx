e8a00df062ace5f2babec4d874a1609a846c4e69
KareemGhorab <kareemghorab3@gmail.com>
2023-08-19 18:44:07 +0300

    feat: 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import NotFound from '@/components/not-found/not-found'
+
+const NotFoundPage: React.FC = (): JSX.Element => (
+	<div className='pt-72'>
+		<NotFound />
+	</div>
+)
+
+export default NotFoundPage
