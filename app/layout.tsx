03cae7102620afa3f0df017bc3de303fab966a27
KareemGhorab <kareemghorab3@gmail.com>
2023-08-24 22:54:09 +0300

    Auth provider applied to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from 'next'
 import { Josefin_Sans } from 'next/font/google'
+import { getServerSession } from 'next-auth'
+
 import './globals.css'
 
-import { config } from '@fortawesome/fontawesome-svg-core'
+import { authOptions } from './api/auth/[...nextauth]/options'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 import Navbar from '@/components/navigation/navbar/navbar'
 import Footer from '@/components/navigation/footer/footer'
+import AuthProvider from '@/context/auth-provider'
 
 const josefin_sans = Josefin_Sans({
 	weight: ['400', '500', '600', '700'],
@@ -19,18 +22,22 @@ export const metadata: Metadata = {
 		'Welcome to FreshMart, your one-stop destination for farm-fresh vegetables and fruits. At FreshMart, we are passionate about bringing you the highest quality produce, handpicked from local farms and delivered straight to your doorstep. Our wide selection includes an array of vibrant vegetables and juicy fruits, carefully cultivated to ensure exceptional taste and nutritional value. With a commitment to freshness and sustainability, we prioritize supporting local farmers and promoting a healthy lifestyle. Explore our online store and experience the joy of wholesome, flavorful produce at FreshMart. Shop with us today and transform your meals into culinary delights!',
 }
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
+type TProps = React.PropsWithChildren
+
+const RootLayout: React.FC<TProps> = async ({ children }: TProps) => {
+	const session = await getServerSession(authOptions)
+
 	return (
 		<html lang='en'>
 			<body className={josefin_sans.className}>
-				<Navbar />
-				{children}
-				<Footer />
+				<AuthProvider>
+					<Navbar />
+					{children}
+					<Footer />
+				</AuthProvider>
 			</body>
 		</html>
 	)
 }
+
+export default RootLayout
