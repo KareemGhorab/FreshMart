fe680a66f6e551f63fbd1689db16c7c3e83fc10e
KareemGhorab <kareemghorab3@gmail.com>
2023-08-26 20:04:40 +0300

    Fixed root layout AuthProvider error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from 'next'
 import { Josefin_Sans } from 'next/font/google'
-import { getServerSession } from 'next-auth'
 
 import './globals.css'
 
-import { authOptions } from './api/auth/[...nextauth]/options'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 import Navbar from '@/components/navigation/navbar/navbar'
 import Footer from '@/components/navigation/footer/footer'
-import AuthProvider from '@/context/auth-provider'
 
 const josefin_sans = Josefin_Sans({
 	weight: ['400', '500', '600', '700'],
@@ -25,16 +23,12 @@ export const metadata: Metadata = {
 type TProps = React.PropsWithChildren
 
 const RootLayout: React.FC<TProps> = async ({ children }: TProps) => {
-	const session = await getServerSession(authOptions)
-
 	return (
 		<html lang='en'>
 			<body className={josefin_sans.className}>
-				<AuthProvider>
-					<Navbar />
-					{children}
-					<Footer />
-				</AuthProvider>
+				<Navbar />
+				{children}
+				<Footer />
 			</body>
 		</html>
 	)
