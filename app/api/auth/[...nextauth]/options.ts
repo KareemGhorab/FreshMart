52a3890af2d0ab6375ee64c3ab1fe08af41ec214
KareemGhorab <kareemghorab3@gmail.com>
2023-08-26 18:26:37 +0300

    Auth API

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,15 +1,18 @@
+import type { NextAuthOptions } from 'next-auth'
+import { object, string } from 'yup'
+import bcrypt from 'bcrypt'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
-import type { NextAuthOptions } from 'next-auth'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 import { prisma } from '@/prisma/db'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { HttpError } from '@/app/api/utils/error-handling/app-error'
 
 export const authOptions: NextAuthOptions = {
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
 		}),
 		CredentialsProvider({
 			name: 'Credentials',
@@ -19,14 +22,42 @@ export const authOptions: NextAuthOptions = {
 					type: 'text',
 				},
 				password: { label: 'Password', type: 'password' },
-				username: { label: 'Username', type: 'text' },
 			},
-			async authorize(credentials, req) {
-				const user = {
-					id: '1',
-					name: 'Kareem',
-					email: 'kareem@gmail.com',
-				}
+			async authorize(credentials) {
+				// Validate the body
+				const schema = object({
+					email: string().required().email(),
+					password: string().required(),
+				})
+				const { email, password } = schema.validateSync(credentials)
+
+				// Check for email existence
+				const user = await prisma.user.findUnique({
+					where: {
+						email,
+					},
+				})
+
+				if (!user || !user.hashedPassword)
+					throw new HttpError(
+						'Login error',
+						"This email isn't registered",
+						404
+					)
+
+				// Check password match
+				const passwordMatch = bcrypt.compareSync(
+					password,
+					user.hashedPassword
+				)
+
+				if (!passwordMatch)
+					throw new HttpError(
+						'Login error',
+						'Incorrect password',
+						400
+					)
+
 				return user
 			},
 		}),
