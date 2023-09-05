f8506ad89cf45b62118d7c9c0c82fe39d6b04769
Kareem Ghorab <kareemghorab3@gmail.com>
2023-09-05 22:14:57 +0300

    Changed server actions into client request

--- app/admin/products/components/new-product.tsx
+++ app/admin/products/components/new-product.tsx
@@ -1,45 +1,73 @@
+'use client'
+
 import clsx from 'clsx'
+import { useFormik } from 'formik'
+import { number, object, string } from 'yup'
 import { TPropsWithClassName } from '@/types/props'
 import { prisma } from '@/prisma/db'
-import { Product } from '@prisma/client'
+
+type TForm = {
+	name: string
+	price: string
+	discount?: string
+	description?: string
+}
+
+const initialValues: TForm = {
+	name: '',
+	price: '',
+	discount: '',
+	description: ''
+}
+
+const schema = object({
+	name: string().required(),
+	description: string().required().min(3),
+	price: number().required().moreThan(0),
+	discount: number(),
+})
+
+const addProduct = async (data: TForm) => {
+	// TODO prisma insert
+}
 
 type TProps = TPropsWithClassName
 
-const addProduct = async (v: FormData) => {
-	'use server'
-	console.log(v)
-	// prisma.product.create({
-	// 	data: {},
-	// })
+const NewProduct: React.FC<TProps> = ({ className }): JSX.Element => {
+	const {handleSubmit, handleChange, errors, dirty } = useFormik<TForm>({
+		onSubmit: addProduct,
+		initialValues: initialValues
+	})
+	return (
+		<form className={clsx('flex flex-col gap-3', className)}>
+			<input
+				placeholder='Product Name'
+				type='text'
+				name='name'
+				id='name'
+			/>
+			<fieldset className='flex gap-3'>
+				<input
+					placeholder='Product Price'
+					type='number'
+					name='price'
+					id='price'
+				/>
+				<input
+					placeholder='Price Discount'
+					type='number'
+					name='discount'
+					id='discount'
+				/>
+			</fieldset>
+			<textarea
+				placeholder='Product Description'
+				name='description'
+				id='description'
+				rows={10}
+			/>
+			<button type='submit'>Submit</button>
+		</form>
+	)
 }
-
-const NewProduct: React.FC<TProps> = ({ className }): JSX.Element => (
-	<form
-		action={addProduct}
-		className={clsx('flex flex-col gap-3', className)}
-	>
-		<input placeholder='Product Name' type='text' name='name' id='name' />
-		<fieldset className='flex gap-3'>
-			<input
-				placeholder='Product Price'
-				type='number'
-				name='price'
-				id='price'
-			/>
-			<input
-				placeholder='Price Discount'
-				type='number'
-				name='discount'
-				id='discount'
-			/>
-		</fieldset>
-		<textarea
-			placeholder='Product Description'
-			name='description'
-			id='description'
-			rows={10}
-		/>
-	</form>
-)
-
 export default NewProduct
