d7cb1b9f956aa67f60058193faf7e5ebae7b51d2
Kareem Ghorab <kareemghorab3@gmail.com>
2023-09-21 19:50:06 +0300

    Removed Google provider

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -2,17 +2,12 @@ import type { NextAuthOptions, Session } from 'next-auth'
 import { object, string } from 'yup'
 import bcrypt from 'bcrypt'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import GoogleProvider from 'next-auth/providers/google'
 
 import { prisma } from '@/prisma/db'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 export const authOptions: NextAuthOptions = {
 	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-		}),
 		CredentialsProvider({
 			name: 'Credentials',
 			credentials: {
